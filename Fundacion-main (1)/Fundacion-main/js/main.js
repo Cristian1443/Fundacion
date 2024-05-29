35c8a5a67ad90d0926786f72d461c812f330008c
Cristian Reyes <140418239+Cristian1443@users.noreply.github.com>
2024-05-29 18:55:24 -0500

    Add files via upload

---
+++ Fundacion-main (1)/Fundacion-main/js/main.js
@@ -0,0 +1,8 @@
+const burger = document.getElementById('burger');
+const navbarCollapse = document.getElementById('navbarNavAltMarkup');
+
+burger.addEventListener("click", () => {
+  navbarCollapse.classList.toggle("show");
+});
+
+
